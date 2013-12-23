d9b0ab412144ab15ccd58726cca7e284b04d4f63
Corey Oliver <corey.jon.oliver@gmail.com>
2013-12-23 11:53:38 -0600

    Adding desired scalac options

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -12,7 +12,16 @@ object BuildSettings {
     startYear := Some(2013),
     licenses := Seq("Apache 2" -> new URL("http://www.apache.org/licenses/LICENSE-2.0.txt")),
     scalaVersion := "2.10.3",
-    resolvers ++= Dependencies.resolutionRepos
+    resolvers ++= Dependencies.resolutionRepos,
+    scalacOptions := Seq(
+      "-encoding", "utf8",
+      "-deprecation",
+      "-target:jvm-1.6", // use the ASM Compiler backend to generate bytecode
+      "-feature", // activate checking of SIP-18 features, forces import language
+      "-unchecked", // enable detailed unchecked warnings
+      "-Xlog-reflective-calls", // print a message when a reflective call is generated
+      "-Ywarn-adapted-args" // warn if an argument list is modified to match the receiver
+    )
   )
 
   lazy val dwollaModuleSettings = basicSettings
