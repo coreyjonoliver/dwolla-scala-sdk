af2bc380f85690aa2fb28ab2360e9d662479d81b
Corey Oliver <corey.jon.oliver@gmail.com>
2013-12-24 19:31:04 -0600

    Testing publishing with TravisCI

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,10 +1,12 @@
 import sbt._
 import Keys._
+import com.typesafe.sbt.SbtPgp
 
 object BuildSettings {
+  val VERSION = "1.0-SNAPSHOT"
 
   lazy val basicSettings = Seq(
-    version := "0.1",
+    version := VERSION,
     homepage := Some(new URL("https://github.com/Dwolla/dwolla-scala")),
     organization := "com.dwolla",
     organizationHomepage := Some(new URL("https://www.dwolla.com")),
@@ -24,5 +26,45 @@ object BuildSettings {
     )
   )
 
-  lazy val dwollaModuleSettings = basicSettings
+  lazy val dwollaModuleSettings =
+    basicSettings ++
+      Seq(
+        // publishing
+        crossPaths := false,
+        publishMavenStyle := true,
+        SbtPgp.useGpg := true,
+        publishTo <<= version {
+          version =>
+            Some {
+              val nexus = "https://oss.sonatype.org/"
+              if (version endsWith "-SNAPSHOT") ("snapshots" at nexus + "content/repositories/snapshots")
+              else ("releases" at nexus + "service/local/staging/deploy/maven2")
+            }
+        },
+        pomIncludeRepository := {
+          _ => false
+        },
+        pomExtra :=
+          <url>https://developers.dwolla.com/dev</url>
+            <licenses>
+              <license>
+                <name>Apache 2</name>
+                <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+                <distribution>repo</distribution>
+              </license>
+            </licenses>
+            <scm>
+              <url>git://github.com/spray/spray.git</url>
+              <connection>scm:git:git@github.com:coreyjonoliver/dwolla-sdk-scala.git</connection>
+            </scm>
+            <developers>
+              <developer>
+                <id>coreyjonoliver</id> <name>Corey Oliver</name>
+              </developer>
+            </developers>,
+        credentials += Credentials("Sonatype Nexus Repository Manager",
+          "oss.sonatype.org",
+          "sys.env(\"DWOLLA_PUBLISH_USERNAME\")",
+          "sys.env(\"DWOLLA_PUBLISH_PASSWORD\")")
+      )
 }
