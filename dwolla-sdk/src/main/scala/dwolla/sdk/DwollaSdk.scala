4ed8732b8c5f952f4021c115ca30f0ec3f104126
Corey Oliver <corey@dwolla.com>
2014-03-03 12:27:29 -0600

    Fix merge conflicts in DwollaSdk.scala

--- dwolla-sdk/src/main/scala/dwolla/sdk/DwollaSdk.scala
+++ dwolla-sdk/src/main/scala/dwolla/sdk/DwollaSdk.scala
@@ -133,20 +133,7 @@ class DwollaSdk(settings: Option[DwollaApiSettings] = None)(
       } yield addFundingSourceResponse
     }
 
-<<<<<<< HEAD
-    def retrieve(accessToken: String, id: String): Future[FundingSource] = {
-=======
-    @deprecated("Use the DwollaSdk.FundingSource.create overload which provides an AccountType parameter", "1.1.1")
-    def create(accessToken: String, accountNumber: String, routingNumber: String, accountType: String,
-               name: String): Future[Models.FundingSource] = {
-      for {
-        addFundingSourceResponse <- dwollaApi.addFundingSource(accessToken, accountNumber, routingNumber,
-          accountType, name)
-      } yield addFundingSourceResponse
-    }
-
     def retrieve(accessToken: String, id: String): Future[Models.FundingSource] = {
->>>>>>> master
       for {
         getFundingSourceDetailsResponse <- dwollaApi.getFundingSourceDetails(accessToken, id)
       } yield getFundingSourceDetailsResponse
